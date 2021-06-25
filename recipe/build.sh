18dedf176514cdd07d5cf741bab765ec3d62efef
Paul Yim <paul.j.yim@gmail.com>
2021-06-25 21:43:42 +0000

    Updates to build on aarch64

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 set -exo pipefail
 
-# Use CC/CXX wrappers to disable -Werror
-export NN_CXX_ORIG=$CXX
-export NN_CC_ORIG=$CC
-export CXX=$RECIPE_DIR/cxx_wrap.sh
-export CC=$RECIPE_DIR/cc_wrap.sh
-
-
 declare -a CMAKE_PLATFORM_FLAGS
 if [[ ${target_platform} == osx-64 ]]; then
   CMAKE_PLATFORM_FLAGS+=(-DCMAKE_OSX_SYSROOT="${CONDA_BUILD_SYSROOT}")
@@ -19,21 +12,46 @@ if [[ $target_platform =~ osx-arm64 ]]; then
   export LDFLAGS="${LDFLAGS} ${CURL_LIBS_APPEND}"
 fi
 
+export CFLAGS="${CFLAGS} -Wno-maybe-uninitialized"
+export CXXFLAGS="${CXXFLAGS} -Wno-maybe-uninitialized"
+
 mkdir build && cd build
-cmake ${CMAKE_ARGS} \
-  -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-  -DCMAKE_PREFIX_PATH=${PREFIX} \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DTILEDB_WERROR=OFF \
-  -DTILEDB_TESTS=OFF \
-  -DTILEDB_INSTALL_LIBDIR=lib \
-  -DTILEDB_HDFS=ON \
-  -DSANITIZER="OFF;-DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE" \
-  -DTILEDB_S3=ON \
-  -DTILEDB_SERIALIZATION=ON \
-  -DTILEDB_LOG_OUTPUT_ON_FAILURE=ON \
-  -DTILEDB_AZURE=ON \
-  "${CMAKE_PLATFORM_FLAGS[@]}" \
-  ..
+
+if [[ $target_platform =~ linux-aarch64 ]]; then
+  # For aarch64, skip the build for Azure (it fails).
+  cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -Wno-error" \
+    -DTILEDB_WERROR=OFF \
+    -DTILEDB_TESTS=OFF \
+    -DTILEDB_INSTALL_LIBDIR=lib \
+    -DTILEDB_HDFS=ON \
+    -DSANITIZER="OFF;-DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE" \
+    -DTILEDB_S3=ON \
+    -DTILEDB_SERIALIZATION=ON \
+    -DTILEDB_LOG_OUTPUT_ON_FAILURE=ON \
+    "${CMAKE_PLATFORM_FLAGS[@]}" \
+    ..
+else
+  cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_PREFIX_PATH=${PREFIX} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS -Wno-error" \
+    -DTILEDB_WERROR=OFF \
+    -DTILEDB_TESTS=OFF \
+    -DTILEDB_INSTALL_LIBDIR=lib \
+    -DTILEDB_HDFS=ON \
+    -DSANITIZER="OFF;-DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE" \
+    -DTILEDB_S3=ON \
+    -DTILEDB_SERIALIZATION=ON \
+    -DTILEDB_LOG_OUTPUT_ON_FAILURE=ON \
+    -DTILEDB_AZURE=ON \
+    "${CMAKE_PLATFORM_FLAGS[@]}" \
+    ..
+fi
+
 make -j ${CPU_COUNT}
 make -C tiledb install
