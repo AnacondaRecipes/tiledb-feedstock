821b2796959aac3b77d2acf3edd494f86b4d3c3f
Kai Tietz <ktietz@anaconda.com>
2021-11-04 11:23:19 +0100

    disable wrapper script on osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -exo pipefail
 
-# Use CC/CXX wrappers to disable -Werror
-export NN_CXX_ORIG=$CXX
-export NN_CC_ORIG=$CC
-export CXX=$RECIPE_DIR/cxx_wrap.sh
-export CC=$RECIPE_DIR/cc_wrap.sh
+if [[ $target_platform == linux-* ]]; then
+  # Use CC/CXX wrappers to disable -Werror
+  export NN_CXX_ORIG=$CXX
+  export NN_CC_ORIG=$CC
+  export CXX=$RECIPE_DIR/cxx_wrap.sh
+  export CC=$RECIPE_DIR/cc_wrap.sh
+fi
 export CMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET}
-chmod +x $CC
-chmod +x $CXX
 
 if [[ $target_platform =~ osx-arm64 ]]; then
   CURL_LIBS_APPEND=`$PREFIX/bin/curl-config --libs`
