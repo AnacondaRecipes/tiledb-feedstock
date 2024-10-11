d4773585e9defdac0c57b473b29a7d5fb605a1e4
Ian Fitchet <ifitchet@anaconda.com>
2024-10-11 18:18:38 +0100

    speculative first build

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -exo pipefail
 
+# Copy tiledb-patches to the source directory to indicate to vcpkg
+# which packages have been installed locally
+cp -r "${RECIPE_DIR}/tiledb-patches/." "${SRC_DIR}"
+
 # Use CC/CXX wrappers to disable -Werror
 export NN_CXX_ORIG=$CXX
 export NN_CC_ORIG=$CC
@@ -27,6 +31,24 @@ if [[ $target_platform == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
 
+# Set the vcpkg target triplet otherwise vcpkg can't find the
+# compilers
+if [[ $target_platform == osx-arm64  ]]; then
+  export VCPKG_TARGET_TRIPLET="arm64-osx"
+fi
+if [[ $target_platform == osx-64  ]]; then
+  export VCPKG_TARGET_TRIPLET="x64-osx"
+fi
+if [[ $target_platform == linux-64  ]]; then
+  export VCPKG_TARGET_TRIPLET="x64-linux"
+fi
+if [[ $target_platform == linux-ppc64le  ]]; then
+  export VCPKG_TARGET_TRIPLET="ppc64le-linux"
+fi
+if [[ $target_platform == linux-aarch64  ]]; then
+  export VCPKG_TARGET_TRIPLET="arm64-linux"
+fi
+
 mkdir build && cd build
 cmake ${CMAKE_ARGS} \
   -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
@@ -35,13 +57,13 @@ cmake ${CMAKE_ARGS} \
   -DTILEDB_TESTS=OFF \
   -DTILEDB_INSTALL_LIBDIR=lib \
   -DTILEDB_HDFS=ON \
-  -DSANITIZER="OFF;-DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE" \
+  -DTILEDB_SANITIZER=OFF \
+  -DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE \
   -DTILEDB_S3=ON \
-  -DTILEDB_SERIALIZATION=ON \
-  -DTILEDB_LOG_OUTPUT_ON_FAILURE=ON \
   -DTILEDB_AZURE=ON \
-  -DCMAKE_ENABLE_WERROR=FALSE \
+  -DTILEDB_SERIALIZATION=ON \
+  -DTILEDB_WEBP=ON \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
   ..
-make -j ${CPU_COUNT}
+make -j $(( ${CPU_COUNT} / 2 + 1 ))
 make -C tiledb install
