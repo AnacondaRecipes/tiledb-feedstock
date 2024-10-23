f7cfdf936389fa057e66d5bfa9f3ea9a18de0c6a
Ian Fitchet <ifitchet@anaconda.com>
2024-10-23 16:22:43 -0500

    WIP

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,26 +1,28 @@
 setlocal EnableDelayedExpansion
 
+REM Copy tiledb-patches to the source directory
+xcopy /Y /S /I "%RECIPE_DIR%\tiledb-patches" "%SRC_DIR%"
+
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
-cmake -G "NMake Makefiles" %CMAKE_ARGS% ^
+cmake -G Ninja %CMAKE_ARGS% ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
       -DTILEDB_WERROR=OFF ^
-      -DTILEDB_TBB_SHARED=ON ^
+      -DTILEDB_TESTS=OFF ^
       -DTILEDB_S3=ON ^
       -DTILEDB_AZURE=ON ^
       -DTILEDB_HDFS=OFF ^
+      -DCOMPILER_SUPPORTS_AVX2=OFF ^
+      -DTILEDB_SKIP_S3AWSSDK_DIR_LENGTH_CHECK=ON ^
       -DTILEDB_SERIALIZATION=ON ^
+      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
+      -DVCPKG_TARGET_TRIPLET=x64-windows ^
+      -DVCPKG_CMAKE_CONFIGURE_OPTIONS=-DCMAKE_FIND_DEBUG_MODE=TRUE ^
       ..
 if errorlevel 1 exit 1
 
-nmake
+cmake --build . -j --target install
 if errorlevel 1 exit 1
-
-pushd "tiledb"
-nmake install
-if errorlevel 1 exit 1
-popd
-
 popd
