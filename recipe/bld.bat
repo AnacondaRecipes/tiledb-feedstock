044c64ac7fb269dc499a9c896eecb00a3723eede
Tomasz Benes <tbenes@anaconda.com>
2024-11-04 19:06:30 +0100

    restore vcpkg for windows

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,6 +6,9 @@ xcopy /Y /S /I "%RECIPE_DIR%\tiledb-patches" "%SRC_DIR%"
 mkdir "%SRC_DIR%"\build
 pushd "%SRC_DIR%"\build
 
+REM Ideally, we'd disable vcpkg as it's done in build.sh
+REM but tiledb is using libmagic (aka darwin file command) on windows, with some patches that make it possible...
+
 cmake -G Ninja %CMAKE_ARGS% ^
       -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
       -DCMAKE_BUILD_TYPE=Release ^
@@ -20,7 +23,6 @@ cmake -G Ninja %CMAKE_ARGS% ^
       -DTILEDB_SERIALIZATION=ON ^
       -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
       -DVCPKG_TARGET_TRIPLET=x64-windows ^
-      -DTILEDB_DISABLE_AUTO_VCPKG=ON ^
       -DVCPKG_CMAKE_CONFIGURE_OPTIONS=-DCMAKE_FIND_DEBUG_MODE=TRUE ^
       ..
 if errorlevel 1 exit 1
