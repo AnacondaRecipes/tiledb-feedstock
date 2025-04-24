d37f5553f854e272b555c952c39b46ec12f9737e
Brian Wing <bwing@anaconda.com>
2025-04-24 11:53:55 -0400

    Build for main channel and Windows

--- recipe/build.sh
+++ recipe/build.sh
@@ -23,10 +23,6 @@ if [[ $target_platform =~ osx-arm64 ]]; then
   export LDFLAGS="${LDFLAGS} ${CURL_LIBS_APPEND}"
 fi
 
-if [[ $target_platform  == linux-64 ]]; then
-  export LDFLAGS="${LDFLAGS} -Wl,--no-as-needed"
-fi
-
 if [[ $target_platform == linux-* ]]; then
   export LDFLAGS="${LDFLAGS} -lrt"
 fi
@@ -49,24 +45,12 @@ if [[ $target_platform == linux-aarch64  ]]; then
   export VCPKG_TARGET_TRIPLET="arm64-linux"
 fi
 
-# For snowflake build only, disabling all the ports:
-# Azure, S3 (AWS) and WebP
-# As they don't require them.
-# TODO: Azure and AWS are supported currently on main in previous version.
-# We should follow up on this package to update it properly and upload to defaults.
-
-# Changes required to build with ports:
-# - TILEDB_AZURE, TILEDB_S3, and optionally TILEDB_WEBP set to ON (WebP currently not supported, as it wasn't back then)
-# - TILEDB_DISABLE_AUTO_VCPKG=ON to disable vcpkg download of dependencies
-# I've been able to find following required packages:
-# - azure-core-cpp
-# - azure-identity-cpp
-# - azure-storage-blobs-cpp
-# - aws-sdk-cpp >=1.11.300 # or >=.2xx, when they introduced generated/src/aws-cpp-sdk-s3/include/aws/s3/model/StorageClass.h with SNOW and EXPRESS_ONEZONE StorageClass values, best to take the commit from vcpkg
-# - capnproto 1.0.1 # hard pinned in cpp code
-# - libmagic
-# Then for azure-storage-blobs-cpp we need azure-storage-core-cpp
-# and for aws-sdk-cpp a chain of aws-c-* dependencies
+# Regenerate the capnp serialization files with the version installed in Conda.
+# This allows updating capnproto independently of upstream tiledb.
+if ! $PREFIX/bin/capnp compile -I $PREFIX/include -oc++:$SRC_DIR/tiledb/sm/serialization $SRC_DIR/tiledb/sm/serialization/tiledb-rest.capnp --src-prefix=$SRC_DIR/tiledb/sm/serialization
+then
+  exit 1
+fi
 
 mkdir build && cd build
 cmake -G Ninja ${CMAKE_ARGS} \
@@ -78,11 +62,12 @@ cmake -G Ninja ${CMAKE_ARGS} \
   -DTILEDB_HDFS=ON \
   -DTILEDB_SANITIZER=OFF \
   -DCOMPILER_SUPPORTS_AVX2:BOOL=FALSE \
-  -DTILEDB_S3=OFF \
-  -DTILEDB_AZURE=OFF \
+  -DTILEDB_S3=ON \
+  -DTILEDB_AZURE=ON \
   -DTILEDB_SERIALIZATION=ON \
-  -DTILEDB_WEBP=OFF \
+  -DTILEDB_WEBP=ON \
   -DTILEDB_DISABLE_AUTO_VCPKG=ON \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET} \
+  -DVCPKG_TARGET_TRIPLET=${VCPKG_TARGET_TRIPLET} \
   ..
 cmake --build . -j $(( ${CPU_COUNT} / 2 + 1 )) --target install
